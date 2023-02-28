92c06081c27dbb3483370c58ddb9f3d2674dc78b
Michal Halabica <m.halabica@gmail.com>
2023-02-28 23:23:48 +0100

    Health checks

--- FileService/Program.cs
+++ FileService/Program.cs
@@ -15,6 +15,9 @@ builder.Services
     .AddSwaggerGen()
     .AddControllers();
 
+builder.Services
+    .AddHealthChecks();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -23,8 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
